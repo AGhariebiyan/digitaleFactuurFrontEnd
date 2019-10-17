bdf7f460f42dd10d3d738e49dcd245a02a74d9a7
alirezaa <alirezaa_afg@live.nl>
2019-10-17 11:38:05 +0200

    controller + model aangemaakt

---
+++ src/controllers/UserController.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import models.UserModel;
+
+public class UserController {
+
+    private UserModel userModel;
+
+    public boolean login (){ return true;}
+    public void getUserLoggedIn(){}
+
+}
